ab47ba80c1a7c0848e069ec7283eeb438de688af
Javier Heisekce <javierheiseckee@gmail.com>
2020-02-26 08:38:47 -0300

    Initial Commit

---
+++ CurrencyMonitor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyMonitor
+//
+//  Created by User on 2/26/20.
+//  Copyright © 2020 jheisecke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
